f6a511d5b2116f35156a6ee9670855e306adcdc5
C3mr <c.3mr@hotmail.com>
2025-07-11 07:03:33 +0300

    Remove duplicate 'use client' directive

--- src/components/home/PropertyMatchCalculator.tsx
+++ src/components/home/PropertyMatchCalculator.tsx
@@ -1,8 +1,6 @@
 
 "use client";
 
-"use client";
-
 import React, { useState, useMemo } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, Controller } from 'react-hook-form';
